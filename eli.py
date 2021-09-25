db811c0e9597d1ce33f307fdba9dc7a730952780
Colt Montana <cwmcphail90@gmail.com>
2021-09-24 22:36:31 -0400

    Updating repo, 3 new files

---
+++ eli.py
@@ -0,0 +1,11 @@
+name = input('ENTER NAME ')
+print ('Hello My Name is, '+ name)
+
+if name == 'Eli':
+    print ('hey eli')
+    ave = input('Eli what is your old Ave? ')
+    
+    if ave == '8':
+        print ('Thats right!')
+    if ave == '9':
+        print ('Thats wrong!')
---
+++ practice.py
@@ -0,0 +1,7 @@
+# This is me practicing coding. I'm going to make a dictionary and some other shit on here.
+
+name = "Colton McPhail"
+age = 31
+city = "Brooklyn"
+birthplace = "Great Falls, MT"
+
