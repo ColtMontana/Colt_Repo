0d65fd074ba2dffd32cf4c8b1bd7753be23b7e6d
Colt Montana <cwmcphail90@gmail.com>
2021-09-24 23:06:20 -0400

    updating personal repo with 2 files: practice and steph

--- practice.py
+++ practice.py
@@ -5,3 +5,11 @@ age = 31
 city = "Brooklyn"
 birthplace = "Great Falls, MT"
 
+input1 = input("What is your name? ")
+
+if input1 == "Colton":
+    print("Hello Colton! Welcome Back to Your Program")
+    print(city)
+how_old = input("I know how old you are, do you wanna see? TYPE yes OR no HERE: ")
+if how_old == "yes":
+    print(age)
---
+++ steph.py
@@ -0,0 +1,15 @@
+# this is going to be a program for my mom
+
+name = "Stephanie"
+
+input1 = (input("How are you feeling today? "))
+
+if input1 == "happy":
+    print("Im so glad you are happy!!!")
+elif input1 == "depressed":
+    print("I am sorry to hear that")
+    input2 = input("can i make you feel better? yes or no? ")
+    if input2 == "yes":
+        print("Cheer up asshole!!")
+    elif input2 == "no":
+        print("Fine, IDC!")
